f34d19b985ed8ba13dac411e914c3fe6f130b06c
Mu1122468 <samuel_buinha@hotmail.com>
2019-07-12 00:27:01 -0300

    fixed routes and css

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\User;
 
 class CreateUsersTable extends Migration
 {
@@ -23,7 +24,7 @@ class CreateUsersTable extends Migration
             $table->timestamps();
         });
 
-        \User::create([
+        User::create([
             'name'      =>  'Admin',
             'email'     =>  'admin@admin.com',
             'password'  =>  'admin'
